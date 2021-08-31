ee8fc7e97476c50cc07683120b6da71f7018ef56
Evan Maddock <maddock.evan@vivaldi.net>
2021-08-31 13:02:28 -0400

    Mobile navmenu fixes
    The old site used spans and magic to animate the toggle button transition. Now, we use SVG icons and forget the transition. This simplifies the whole thing massively.
    
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { NavbarData } from './navbarData';
+import { slideToggle } from './slider';
 import logo from '../logo_white.png';
 import './Navbar.css';
 
@@ -13,25 +14,40 @@ interface NavbarToggleProps {
 
 const NavbarToggle = (props: NavbarToggleProps): JSX.Element => {
   const {isShowing, onClick} = props;
+  const classes = `md:hidden absolute cursor-pointer p-2 right-1 top-14`;
 
   if (isShowing) {
     return (
-      <Link to="#" id="navbar-toggle" className="hidden absolute right-0 top-16 w-16 h-16" onClick={onClick}>
-        <AiIcons.AiOutlineClose />
+      <Link to="#" id="navbar-toggle" className={classes} onClick={onClick}>
+        <AiIcons.AiOutlineClose className="w-10 h-10" />
       </Link>
     )
   } else {
     return (
-      <Link to="#" id="navbar-toggle" className="navbar-toggle absolute right-0 top-16 w-16 h-16" onClick={onClick}>
-        <FaIcons.FaBars />
+      <Link to="#" id="navbar-toggle" className={classes} onClick={onClick}>
+        <FaIcons.FaBars className="w-10 h-10" />
       </Link>
     )
   }
 }
 
 const Navbar = (): JSX.Element => {
-  const [sidebar, setSidebar] = useState(false);
-  const showSidebar = () => setSidebar(!sidebar);
+  const [navOpen, setNavOpen] = useState(false);
+  const toggleNavMenu = () => {
+    const toggle = document.getElementById('navbar-toggle')
+    if (!toggle) {
+      return;
+    }
+
+    const menu = document.getElementById('nav-list')
+    if (!menu) {
+      return;
+    }
+
+    toggle.classList.toggle('active');
+    slideToggle(menu, navOpen, 300);
+    setNavOpen(!navOpen);
+  };
 
   return (
       <section className="h-nav">
@@ -41,15 +57,16 @@ const Navbar = (): JSX.Element => {
           </div>
 
           <nav className="float-right">
-            <div className="nav-mobile absolute hidden top-16 right-0 w-16 h-16">
-              <NavbarToggle isShowing={sidebar} onClick={showSidebar} />
-            </div>
-            <ul id="nav-list" className="list-none m-0 p-0" onClick={showSidebar}>
+            {/* <div className="block md:hidden absolute top-16 right-0 w-16 h-16">
+              <NavbarToggle isShowing={navOpen} onClick={toggleNavMenu} />
+            </div> */}
+            <NavbarToggle isShowing={navOpen} onClick={toggleNavMenu} />
+            <ul id="nav-list" className="list-none hidden m-0 p-0 md:block md:pt-16" onClick={toggleNavMenu}>
               {NavbarData.map((item, index) => {
                 // Map each navbar item info to a link element
                 return (
-                  <li key={index} className={"float-left font-lighter lowercase no-underline p-0 relative"}>
-                    <Link to={item.path} className="navbar-link block pt-1.5">
+                  <li key={index} className={"float-none md:float-left font-lighter lowercase no-underline p-0 relative md:mx-5 md:my-8"}>
+                    <Link to={item.path} className="navbar-link block md:pt-1.5">
                       {item.title}
                     </Link>
                   </li>
---
+++ src/components/slider.tsx
@@ -0,0 +1,70 @@
+export function slideUp(target: HTMLElement, duration: number) {
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = `${duration}ms`;
+  target.style.boxSizing = 'border-box';
+  target.style.height = `${target.offsetHeight}px`;
+
+  window.setTimeout(() => {
+    target.style.height = '0';
+    target.style.paddingTop = '0';
+    target.style.paddingBottom = '0';
+    target.style.marginTop = '0';
+    target.style.marginBottom = '0';
+    target.style.overflow = 'hidden';
+
+    window.setTimeout(() => {
+      target.style.display = 'none';
+      target.style.removeProperty('height');
+      target.style.removeProperty('padding-top');
+      target.style.removeProperty('padding-bottom');
+      target.style.removeProperty('margin-top');
+      target.style.removeProperty('margin-bottom');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+    }, duration);
+  }, 10);
+}
+
+export function slideDown(target: HTMLElement, duration: number) {
+  target.style.removeProperty('display');
+  let { display } = window.getComputedStyle(target);
+  if (display === 'none') {
+    display = 'block';
+  }
+  target.style.display = display;
+
+  const { offsetHeight } = target;
+  target.style.height = '0';
+  target.style.paddingTop = '0';
+  target.style.paddingBottom = '0';
+  target.style.marginTop = '0';
+  target.style.marginBottom = '0';
+  target.style.overflow = 'hidden';
+
+  target.style.boxSizing = 'border-box';
+  target.style.transitionProperty = 'height, margin, padding';
+  target.style.transitionDuration = `${duration}ms`;
+  window.setTimeout(() => {
+    target.style.height = `${offsetHeight}px`;
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+
+    window.setTimeout(() => {
+      target.style.removeProperty('height');
+      target.style.removeProperty('overflow');
+      target.style.removeProperty('transition-duration');
+      target.style.removeProperty('transition-property');
+    }, duration);
+  }, 10);
+}
+
+export function slideToggle(target: HTMLElement, visible: boolean, duration: number = 500) {
+  if (visible) {
+    slideUp(target, duration);
+  } else {
+    slideDown(target, duration);
+  }
+}
