8b19afd9ed60e5ddad11e725f1792a7ede338883
Evan Maddock <maddock.evan@vivaldi.net>
2021-11-05 20:41:17 -0400

    Set the role property for our alert container
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- src/components/AlertContainer.tsx
+++ src/components/AlertContainer.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { AlertProps, alertService } from '../services/alert.service';
-import Alert from './Alert';
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { AlertProps, alertService } from "../services/alert.service";
+import Alert from "./Alert";
 
 interface AlertContainerProps {
-  id: string,
+  id: string;
 }
 
 const AlertContainer = (props: AlertContainerProps) => {
@@ -14,22 +14,20 @@ const AlertContainer = (props: AlertContainerProps) => {
 
   useEffect(() => {
     // Subscribe to alert notifications
-    const subscription = alertService.onAlert(id)
-      .subscribe(alert => {
-        // Clear all alerts if we get an empty one
-        if (!alert.message) {
-          setAlerts([]);
-          return;
-        }
-
-        // Add alert to array
-        setAlerts(alerts => ([...alerts, alert]));
-
-        if (alert.autoclose) {
-          setTimeout(() => removeAlert(alert), 5000);
-        }
+    const subscription = alertService.onAlert(id).subscribe((alert) => {
+      // Clear all alerts if we get an empty one
+      if (!alert.message) {
+        setAlerts([]);
+        return;
       }
-    );
+
+      // Add alert to array
+      setAlerts((alerts) => [...alerts, alert]);
+
+      if (alert.autoclose) {
+        setTimeout(() => removeAlert(alert), 5000);
+      }
+    });
 
     // Clear alerts on location change
     const historyUnlisten = history.listen(() => {
@@ -46,20 +44,30 @@ const AlertContainer = (props: AlertContainerProps) => {
 
   function removeAlert(alert: AlertProps) {
     const alertWithFade = { ...alert, fade: true };
-    setAlerts(alerts => alerts.map(obj => obj === alert ? alertWithFade : obj));
+    setAlerts((alerts) =>
+      alerts.map((obj) => (obj === alert ? alertWithFade : obj))
+    );
 
     setTimeout(() => {
-      setAlerts(alerts => alerts.filter(obj => obj !== alertWithFade));
+      setAlerts((alerts) => alerts.filter((obj) => obj !== alertWithFade));
     }, 500);
   }
 
   return (
-    <div className="container lg:max-w-3xl mx-auto py-2">
+    <div className="container lg:max-w-3xl mx-auto py-2" role="alert">
       {alerts.map((alert: AlertProps, index: number) => {
-        return <Alert key={index} type={alert.type} message={alert.message} fade={alert.fade} onClose={() => removeAlert(alert)} />
+        return (
+          <Alert
+            key={index}
+            type={alert.type}
+            message={alert.message}
+            fade={alert.fade}
+            onClose={() => removeAlert(alert)}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 export default AlertContainer;
